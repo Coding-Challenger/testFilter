e6db2e50c7604bd447a3d35dda977ef80d6e4af1
David Costa <davidcosta71@gmail.com>
2025-07-03 22:39:05 -0300

    Atualização de relacionamentos de models e ajuste no docker-compose para suporte do node/npm

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,9 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Product extends Model
 {
-    /** @use HasFactory<\Database\Factories\ProductFactory> */
     use HasFactory;
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
